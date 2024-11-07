4303d98ae2291d3d1958af39dae793ce0ca7e939
kentangkiting <agustinus_bintang@yahoo.com>
2024-11-07 18:56:16 +0700

    homework28_2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import viteLogo from "/vite.svg";
 import NewComponent from "./component/newComponent.jsx";
 import "./App.css";
 
+const ItemComponent = ({ nama, price }) => {
+  return (
+    <div>
+      <h2>{nama}</h2>
+      <p>Rp. {price}</p>
+    </div>
+  );
+};
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,6 +20,8 @@ function App() {
     <>
       Hello
       <NewComponent props={"passing value via Props"} />
+      <ItemComponent nama={"bintang"} price={"20.000"} />
+      <ItemComponent nama={"bulan"} price={"30.000"} />
     </>
   );
 }
